70fb7315a664c42a98de7c09faef5ef52a27f582
duytu0410 <38678093+duytu0410@users.noreply.github.com>
2020-03-05 12:04:38 +0700

    5/3

--- Desktop/Angular/A1/A1/src/app/app.module.ts
+++ Desktop/Angular/A1/A1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { Login2Component } from './components/login2/login2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    Login2Component,
 
   ],
   imports: [
